fe2af113abce17145242e05243c07f39f1b669d4
jiwonpark <jiwon3401@gmail.com>
2023-04-17 17:15:08 +0900

    update

---
+++ models/SBertEncoder.py
@@ -0,0 +1,62 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+
+from transformers import AutoTokenizer, AutoModel
+from transformers import BertModel, BertTokenizer, GPT2Model, GPT2Tokenizer,\
+    RobertaModel, RobertaTokenizer, DistilBertModel, DistilBertTokenizer,\
+    CLIPTokenizer, CLIPTextModel
+from sentence_transformers import SentenceTransformer
+
+
+from models.BERT_Config import MODELS
+
+
+
+class SenBERTEncoder(nn.Module):
+    def __init__(self, config):
+        super(SenBERTEncoder, self).__init__()
+        
+        # bert_type = config.bert_encoder.type #settings.yaml에서 'sentence-transformers/all-MiniLM-L6-v2' 로 바꿔야함 ㄴㄴ
+        dropout = 0.1
+        
+        self.tokenizer = AutoTokenizer.from_pretrained('sentence-transformers/all-MiniLM-L6-v2')
+        self.bert_encoder = AutoModel.from_pretrained('sentence-transformers/bert-base-nli-mean-tokens', add_pooling_layer = False) #pooling_layer 제외
+        
+        self.text_width = 768
+        
+        '''
+        freeze = True
+        if freeze:
+        '''
+        if config.training.freeze:
+            for name, param in self.bert_encoder.named_parameters():
+                param.requires_grad = False
+        else:
+            for name, param in self.bert_encoder.named_parameters():
+                param.requires_grad = True
+                
+        
+    def forward(self, caption):
+        device = torch.device('cuda')
+        
+        text_input = self.tokenizer(caption, add_special_tokens=True, truncation=True,
+                                    padding=True, return_tensors='pt').to(device)
+        
+        
+        text_output = self.bert_encoder(input_ids = text_input.input_ids,
+                                        attention_mask = text_input.attention_mask)[0]
+        
+        cls = text_output[:,0,:]
+        
+        return cls
+                                
+        
+        
+
+#         self.tokenizer = AutoTokenizer.from_pretrained()
+#         self.model = AutoModel.from_pretrained('sentence-transformers/all-mpnet-base-v2')
+        
---
+++ tools/info_loss.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+from sentence_transformers import util
+
+
+
+class InFoNCELoss(nn.Module):
+    
+    def __init__(self, temperature=0.07):
+        super(InFoNCELoss, self).__init__()
+        
+        self.loss = nn.CrossEntropyLoss()
+        self.tau = temperature
+        
+    
+    def forward(self, audio_embeds, text_embeds, labels):
+        """
+        :param audio_embeds: tensor, (N,E)
+        :param text_embeds
+        :param labels(item_batch) # audio-text info 
+        :return:
+        """
+        
+        n = audio_embeds.size(0) # 배치 사이즈
+              
+        
+        a2t = util.cos_sim(audio_embeds, text_embeds) / self.tau
+        t2a = util.cos_sim(text_embeds, audio_embeds) / self.tau
+        
+        labels = torch.arange(n).to(a2t.device)
+        
+        a2t_loss = self.loss(a2t, labels)
+        t2a_loss = self.loss(t2a, labels)
+        
+
+        loss = (a2t_loss + t2a_loss)/2
+        
+        return loss
+    
