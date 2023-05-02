5d0864997d94fe1724d3b51abbc5a207d34d7b05
sangje <parksj2480@naver.com>
2023-05-02 20:28:43 +0900

    Top5 > Top10

--- tools/make_csvfile.py
+++ tools/make_csvfile.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
 
-def make_csv(caption_names, audio_names_, top5_index, csv_output_dir):
-    df_rank=pd.DataFrame([audio_names_[i.astype(int)] for i in top5_index],index=caption_names)
+def make_csv(caption_names, audio_names_, top10_index, csv_output_dir):
+    df_rank=pd.DataFrame([audio_names_[i.astype(int)] for i in top10_index],index=caption_names)
     df_rank.to_csv(csv_output_dir.joinpath('results.csv'),index=True)
 
 
