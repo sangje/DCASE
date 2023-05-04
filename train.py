f0d8e631be6e730e5a551453857ac5de94227505
sangje <parksj2480@naver.com>
2023-05-04 10:24:53 +0900

    Tokenizers paraellism false

--- train.py
+++ train.py
@@ -14,6 +14,7 @@ import warnings
 if __name__ == '__main__':
     warnings.filterwarnings("ignore")
     os.environ['TF_ENABLE_ONEDNN_OPTS']='0'
+    os.environ['TOKENIZERS_PARALLELISM'] = 'false'
 
     parser = argparse.ArgumentParser(description='Settings.')
     parser.add_argument('-n', '--exp_name', default='exp_name', type=str,
