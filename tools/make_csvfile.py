5033543006acbdb5843ca53d496732a2c02b1a23
sangje <parksj2480@naver.com>
2023-05-08 16:53:11 +0900

    let's find error

--- tools/make_csvfile.py
+++ tools/make_csvfile.py
@@ -2,6 +2,10 @@ import pandas as pd
 import numpy as np
 
 def make_csv(caption_names, audio_names_, top10_index, csv_output_dir):
+    print(caption_names[:5])
+    print(audio_names_[:5])
+    print(top10_index[:5])
+    print(csv_output_dir)
     df_rank=pd.DataFrame([audio_names_[i.astype(int)] for i in top10_index],index=caption_names)
     df_rank.to_csv(csv_output_dir.joinpath('results.csv'),index=True)
 
