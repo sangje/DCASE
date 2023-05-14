f5edbed825c006fadcece682b788068c6bf685a5
sangje <parksj2480@naver.com>
2023-05-15 01:40:59 +0900

    make_csvfile modified - v3

--- tools/make_csvfile.py
+++ tools/make_csvfile.py
@@ -4,6 +4,6 @@ import numpy as np
 def make_csv(caption_names, audio_names_, top10_index, csv_output_dir):
     df_rank=pd.DataFrame([audio_names_[i.astype(int)] for i in top10_index],index=caption_names)
     df_rank.columns=["fname_1","fname_2","fname_3","fname_4","fname_5","fname_6","fname_7","fname_8","fname_9","fname_10"]
-    df_rank.to_csv(csv_output_dir.joinpath('results.csv'),index=True)
+    df_rank.to_csv('../'+csv_output_dir.joinpath('results.csv'),index=True)
 
 
