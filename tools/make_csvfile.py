14eefe49d08fd7d57707ed31b3d731cbd1b50f50
sangje <parksj2480@naver.com>
2023-05-15 01:43:21 +0900

    make_csvfile modified - v5

--- tools/make_csvfile.py
+++ tools/make_csvfile.py
@@ -6,6 +6,7 @@ from pathlib import Path
 def make_csv(caption_names, audio_names_, top10_index, csv_output_dir):
     df_rank=pd.DataFrame([audio_names_[i.astype(int)] for i in top10_index],index=caption_names)
     df_rank.columns=["fname_1","fname_2","fname_3","fname_4","fname_5","fname_6","fname_7","fname_8","fname_9","fname_10"]
-    df_rank.to_csv(Path(csv_output_dir,'results.csv'),index=True)
+    df_rank.to_csv(Path('../',csv_output_dir,'results.csv'),index=True)
+    print("Saved CSV file")
 
 
