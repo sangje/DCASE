1ff0efa6af7f05b225f41101ab2750f5f555a28b
sangje <parksj2480@naver.com>
2023-05-15 19:54:47 +0900

    Double check the csv_output_dir - v5

--- tools/make_csvfile.py
+++ tools/make_csvfile.py
@@ -3,11 +3,10 @@ import numpy as np
 from pathlib import Path
 
 
-def make_csv(caption_names, audio_names_, top10_index, csv_output_dir):
+def make_csv(caption_names, audio_names_, top10_index, csv_output_path):
     df_rank=pd.DataFrame([audio_names_[i.astype(int)] for i in top10_index],index=caption_names)
     df_rank.columns=["fname_1","fname_2","fname_3","fname_4","fname_5","fname_6","fname_7","fname_8","fname_9","fname_10"]
-    csv_output_path = Path(csv_output_dir,'result.csv')
     df_rank.to_csv(csv_output_path, index=True)
-    print("Saved CSV file at {}".format(csv_output_dir))
+    print("Saved CSV file at {}".format(csv_output_path))
 
 
